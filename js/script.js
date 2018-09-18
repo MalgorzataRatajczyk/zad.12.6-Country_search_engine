00d7e0103f79d04ed6d22197f7de75c60e05f953
Malgorzata Ratajczyk <malgorzataratajczyk@wp.pl>
2018-09-18 22:31:47 +0200

    Add js code to get informmation about countries

--- js/script.js
+++ js/script.js
@@ -27,5 +27,25 @@ function showCountriesList(resp) {
         var liEl = document.createElement('li');
         liEl.innerText = item.name;
         countriesList.appendChild(liEl);
+
+        var liE2 = document.getElementById('continent');
+        liE2.innerText = item.region;
+
+        var liE3 = document.getElementById('capital');
+        liE3.innerText = item.capital;
+    
+        var liE4 = document.getElementById('area');
+        liE4.innerText = item.area;
+      
+        var liE5 = document.getElementById('population');
+        liE5.innerText = item.population;
+
+        var liE6 = document.getElementById('language');
+        liE6.innerText = item.languages;
+
+        var liE7 = document.getElementById('currency');
+        liE7.innerText = item.currencies;
+
     });
+
   }
